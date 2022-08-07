71d990610ee3c43a44c8487b1431682dbec226ef
krutoi5 <zedbym@gmail.com>
2022-08-07 16:58:15 +0300

    добывил метотд

--- lesson.py
+++ lesson.py
@@ -1,3 +1,4 @@
+
 class Car():
     def __init__(self, color, prise, age, max_speed):
         self.color = color
@@ -25,7 +26,7 @@ class Car():
         color = 'widht'
         prise = self.prise - other.prise
         age = self.age - other.age
-        max_speed =self.max_speed - other.max_speed
+        max_speed = self.max_speed - other.max_speed
         return Car(color, prise, age, max_speed)
     
     def __mul__(self, other):
@@ -34,6 +35,13 @@ class Car():
         age = self.age * other.age
         max_speed =self.max_speed * other.max_speed
         return Car(color, prise, age, max_speed)
+    
+    def __floordiv__(self, other):
+        color = 'green'
+        prise = self.prise // other.prise
+        age = self.age // other.age
+        max_speed = self.max_speed // other.max_speed
+        return Car(color, prise, age, max_speed)
 
 
 car1 = Car('red', 2000000, 3, 280)
@@ -41,10 +49,10 @@ car2 = Car('red', 1000000, 2, 190)
 
 car3 = car1 - car2
 car4 = car1 * car2
+car5 = car1 // car2
 
-print(car3, car4)
+print(car3, car4, car5)
 if car1 == car2:
     print('Одинаковые')
 else:
     print('разные')
-#print(car1)
